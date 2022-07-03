166b822186ba3b219aae29b47abe856aa8eae74e
k <pesarxboy@gmail.com>
2022-07-03 20:07:38 +0430

    change design

---
+++ script.js
@@ -0,0 +1,7 @@
+// const colors = ["#414a4c", "#2C3639", "#1B2430", "#2F8F9D"];
+// const i = Math.trunc(Math.random() * colors.length);
+
+// document.body.style.backgroundColor = colors[i];
+// document
+//   .querySelector("meta[name='theme-color']")
+//   .setAttribute("content", colors[i]);
